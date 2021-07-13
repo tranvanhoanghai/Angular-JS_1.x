6ef187283c8d7ade14b8fa5d601bb75bc1c44316
tranvanhoanghai <74179524+tranvanhoanghai@users.noreply.github.com>
2021-07-13 18:01:55 +0700

    add comment

--- tryAngularJs/app/app.js
+++ tryAngularJs/app/app.js
@@ -25,56 +25,6 @@ app.config([
     cssInjectorProvider.setSinglePageMode(true);
     $qProvider.errorOnUnhandledRejections(false);
 
-    // $urlRouterProvider.otherwise("/home"); //Mọi đường dẫn không hợp lệ đều được chuyển đến state home
-
-    // $stateProvider
-    //   .state("dashboard", {
-    //     url: "/dashboard",
-    //     template: "<dashboard></dashboard>",
-    //   })
-    //   .state("login", {
-    //     url: "/login",
-    //     template: "<login></login>",
-    //   })
-    //   .state("contact", {
-    //     url: "/contact",
-    //     template: "<contact></contact>",
-    //     // reloadOnSearch: false,
-    //   })
-    //   .state("contact.create", {
-    //     url: "/create",
-    //     template: "<create-contact></create-contact>",
-    //   })
-    //   .state("contact.edit", {
-    //     url: "/contact/edit/:id",
-    //     template: "<edit-contact></edit-contact>",
-    //   })
-    //   .state("sales-order", {
-    //     url: "/sales-order",
-    //     template: "<sales-order></sales-order>",
-    //     reloadOnSearch: false,
-    //   })
-    //   .state("sales-order.create", {
-    //     url: "/sales-order/create",
-    //     template: "<create-sales-order></create-sales-order>",
-    //   })
-    //   .state("sales-order.edit", {
-    //     url: "/sales-order/edit/:id",
-    //     template: "<edit-sales-order></edit-sales-order>",
-    //   })
-    //   .state("user", {
-    //     url: "/user",
-    //     template: "<user></user>",
-    //   })
-    //   .state("user.create", {
-    //     url: "/user/create",
-    //     template: "<create-user></create-user>",
-    //   })
-    //   .state("user.edit", {
-    //     url: "/user/edit/:id",
-    //     template: "<edit-user></edit-user>",
-    //   });
-
     $routeProvider
       .when("/", {
         template: "<dashboard></dashboard>", //(<view1></view1> is name component)
